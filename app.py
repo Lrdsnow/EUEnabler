bee4945e3a8bc06e10ce6dd46e16b6cf08d4ee6a
Lrdsnow <63671193+Lrdsnow@users.noreply.github.com>
2024-09-05 18:19:58 -0700

    oops

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from exploit.restore import restore_file
+from exploit.restore import restore_files, FileToRestore, restore_file
+
+from pymobiledevice3.exceptions import PyMobileDevice3Exception
+from pymobiledevice3.services.diagnostics import DiagnosticsService
+from pymobiledevice3 import usbmux
+from pymobiledevice3.lockdown import create_using_usbmux
+
 from pathlib import Path
+from tempfile import TemporaryDirectory
 import plistlib
+import traceback
 
 region_code = input("Enter YOUR 2-letter region code (default to US): ").strip().upper() or "US"
-method = int(input("Choose a method, 1 or 2:")) or 1
+method = int(input("Choose a method, 1 or 2: ") or "1") or 1
+print("Please wait...")
 
 def replace_region_code(plist_path, original_code="US", new_code="US"):
     with open(plist_path, 'rb') as f:
@@ -13,20 +22,52 @@ def replace_region_code(plist_path, original_code="US", new_code="US"):
     updated_plist_str = plist_str.replace(original_code, new_code)
     updated_plist_data = eval(updated_plist_str)  # Convert string back to dictionary
 
-    with open(plist_path, 'wb') as f:
-        plistlib.dump(updated_plist_data, f)
+    return plistlib.dumps(updated_plist_data)
+
+
 
 file_path = Path.joinpath(Path.cwd(), 'eligibility.plist')
-if region_code != "US":
-    replace_region_code(file_path, original_code="US", new_code=region_code)
-restore_file(fp=file_path, restore_path='/var/db/os_eligibility/', restore_name='eligibility.plist')
+eligibility_data = replace_region_code(file_path, original_code="US", new_code=region_code)
+
+files_to_restore = [
+    FileToRestore(
+        contents=eligibility_data,
+        restore_path="/var/db/os_eligibility/",
+        restore_name="eligibility.plist"
+    )
+]
 
 file_path = Path.joinpath(Path.cwd(), 'Config.plist')
-if region_code != "US":
-    replace_region_code(file_path, original_code="US", new_code=region_code)
+config_data = replace_region_code(file_path, original_code="US", new_code=region_code)
 if method == 1:
-    restore_file(fp=file_path, restore_path='/var/MobileAsset/AssetsV2/com_apple_MobileAsset_OSEligibility/purpose_auto/c55a421c053e10233e5bfc15c42fa6230e5639a9.asset/AssetData/', restore_name='Config.plist')
+    files_to_restore.append(
+        FileToRestore(
+            contents=config_data,
+            restore_path="/var/MobileAsset/AssetsV2/com_apple_MobileAsset_OSEligibility/purpose_auto/c55a421c053e10233e5bfc15c42fa6230e5639a9.asset/AssetData/",
+            restore_name="Config.plist"
+        )
+    )
 elif method == 2:
-    restore_file(fp=file_path, restore_path='/var/MobileAsset/AssetsV2/com_apple_MobileAsset_OSEligibility/purpose_auto/247556c634fc4cc4fd742f1b33af9abf194a986e.asset/AssetData/', restore_name='Config.plist')
+    files_to_restore.append(
+        FileToRestore(
+            contents=config_data,
+            restore_path="/var/MobileAsset/AssetsV2/com_apple_MobileAsset_OSEligibility/purpose_auto/247556c634fc4cc4fd742f1b33af9abf194a986e.asset/AssetData/",
+            restore_name="Config.plist"
+        )
+    )
 
-print("Reboot to see changes!")
+# taken from nugget
+try:
+    restore_files(files=files_to_restore)
+except PyMobileDevice3Exception as e:
+    if "Find My" in str(e):
+        print("Find My must be disabled in order to use this tool.")
+        print("Disable Find My from Settings (Settings -> [Your Name] -> Find My) and then try again.")
+    elif "crash_on_purpose" not in str(e):
+        raise e
+    else:
+        print("Applied, Please reboot to see changes!")
+except Exception as e:
+    print(traceback.format_exc())
+finally:
+    input("Press Enter to exit...")
