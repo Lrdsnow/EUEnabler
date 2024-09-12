870d55fee47cb8de7e210c48fad8c64a5d7bc22e
Lrdsnow <63671193+Lrdsnow@users.noreply.github.com>
2024-09-12 05:17:23 -0700

    fully fixed

--- app.py
+++ app.py
@@ -7,28 +7,16 @@ from pymobiledevice3.lockdown import create_using_usbmux
 
 from pathlib import Path
 from tempfile import TemporaryDirectory
-import plistlib
 import traceback
 
-region_code = input("Enter YOUR CURRENT 2-letter region code (default to US): ").strip().upper() or "US"
 print("Please wait...")
 
-def replace_region_code(plist_path, original_code="US", new_code="US"):
-    with open(plist_path, 'rb') as f:
-        plist_data = plistlib.load(f)
-    
-    plist_str = str(plist_data)
-    updated_plist_str = plist_str.replace(original_code, new_code)
-    updated_plist_data = eval(updated_plist_str)  # Convert string back to dictionary
-
-    return plistlib.dumps(updated_plist_data)
-
-
-
 file_path = Path.joinpath(Path.cwd(), 'eligibility.plist')
-eligibility_data = replace_region_code(file_path, original_code="US", new_code=region_code)
+with open(file_path, 'rb') as file:
+    eligibility_data = file.read()
 file_path = Path.joinpath(Path.cwd(), 'Config.plist')
-config_data = replace_region_code(file_path, original_code="US", new_code=region_code)
+with open(file_path, 'rb') as file:
+    config_data = file.read()
 
 files_to_restore = [
     FileToRestore(
@@ -39,16 +27,6 @@ files_to_restore = [
     FileToRestore(
         contents=config_data,
         restore_path="/var/MobileAsset/AssetsV2/com_apple_MobileAsset_OSEligibility/purpose_auto/c55a421c053e10233e5bfc15c42fa6230e5639a9.asset/AssetData/",
-        restore_name="Config.plist"
-    ),
-    FileToRestore(
-        contents=config_data,
-        restore_path="/var/MobileAsset/AssetsV2/com_apple_MobileAsset_OSEligibility/purpose_auto/247556c634fc4cc4fd742f1b33af9abf194a986e.asset/AssetData/",
-        restore_name="Config.plist"
-    ),
-    FileToRestore(
-        contents=config_data,
-        restore_path="/var/MobileAsset/AssetsV2/com_apple_MobileAsset_OSEligibility/purpose_auto/250df115a1385cfaad96b5e3bf2a0053a9efed0f.asset/AssetData/",
         restore_name="Config.plist"
     )
 ]
