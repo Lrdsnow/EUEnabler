5cb366212ea91b0923af9acca8f6e4036887b7d1
Lrdsnow <63671193+Lrdsnow@users.noreply.github.com>
2024-09-12 08:03:51 -0700

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ except PyMobileDevice3Exception as e:
         print("Find My must be disabled in order to use this tool.")
         print("Disable Find My from Settings (Settings -> [Your Name] -> Find My) and then try again.")
     if "File exists" in str(e):
-        print("It seems you tried to run this again without rebooting, please reboot before trying again.")
+        print("It seems you tried to run this again without rebooting your phone, please reboot before trying again.")
     #if "link error" in str(e):
         #print("Failed to connect to device, if you are on windows ensure you dont have any app made by apple installed from the microsoft store and ensure your device is recognized by itunes")
     elif "crash_on_purpose" not in str(e):
