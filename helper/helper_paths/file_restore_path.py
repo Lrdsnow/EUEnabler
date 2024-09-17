b896e4861369844e7185cdc4ba2970b6ac60e901
nightfallenxyz <160358800+nightfallenxyz@users.noreply.github.com>
2024-09-16 20:53:33 -0400

    Paths Fix/Update
    .

--- helper/helper_paths/file_restore_path.py
+++ helper/helper_paths/file_restore_path.py
@@ -12,6 +12,7 @@ files_to_restore_empty = [  # Empty restore files
         contents=b'',
         restore_path="/var/MobileAsset/AssetsV2/com_apple_MobileAsset_OSEligibility/purpose_auto/c55a421c053e10233e5bfc15c42fa6230e5639a9.asset/AssetData/",
         restore_name="Config.plist"
+),
 ]
 
 files_to_restore_patches = [  # Files to apply eligibility and config patches
